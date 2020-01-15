f149900b39127866c19fb0af7fe9ab0827984f32
Nitesh Turaga <nitesh.turaga@gmail.com>
2020-01-14 23:26:33 -0500

    TODO:
    1. docker api is broken, specifically the function to list all the
    images
    
    2. Documentation is needed
    
    3. There is a problem with the way readLines reads the && operators.

--- R/docker_api.R
+++ R/docker_api.R
@@ -1,3 +1,5 @@
+#' Docker command line function to run with arguments
+#'
 .docker_do <- function(args) {
     err <- system2("docker", args, wait = TRUE)
     if (err)
@@ -18,30 +20,37 @@
 }
 
 .docker_get <-
-    function(path, api="https://hub.docker.com/v2")
+    function(path, api = "https://hub.docker.com/v2",
+             path_root = "/repositories")
 {
-    query <- sprintf("%s%s", api, path)
+    query <- sprintf("%s%s%s", api, path, path_root)
     .docker_get_query(query)
 }
 
+#' Get Docker image pull count (number of times downloaded)
 #' 
-docker_get_pull_count <-
+#'  @param `character(1)` docker image name with organization 
+docker_image_pull_count <-
     function(image = character(1))
 {
-    .docker_get(sprintf("/repositories/%s", image))$pull_count
+    .docker_get(image)$pull_count
 }
 
-
-docker_list_bioconductor_images <-
-    function()
+#' 
+docker_image_list <-
+    function(organization = character(1))
 {
-    query <- .docker_get("/repositories/bioconductor/?page_size=100")
-    paste0("bioconductor/", sapply(query$results, `[[`, "name"))
+    bioc <- .docker_get(path = sprintf("%s/?page_size=100", organization),
+                        path_root = "") 
+    paste0("bioconductor/", sapply(bioc$results, `[[`, "name"))
 }
 
-
-docker_get_description <-
+#' Get docker image description
+#' 
+#' @param `character(1)` image name with organization name
+docker_image_description <-
     function(image=character(1))
 {
-    .docker_get(sprintf("/repositories/%s", image))$description
+    .docker_get(image)$description
 }
+
--- R/template.R
+++ R/template.R
@@ -8,8 +8,8 @@
 #'
 #' @return `character(1)` version number or name (devel).
 #' 
-get_rocker_rstudio_version <-
-    function(version = c("devel", "release"))
+.get_rocker_rstudio_version <- 
+    function(version = c("devel", "release")) 
 {
     version <- match.arg(version)
     CONFIG <- system.file("extdata", "config", package="BiocDockerManager")
@@ -20,7 +20,8 @@ get_rocker_rstudio_version <-
 
 #' Get dockerfile template
 #'
-#' @details The template is annotated with whisker templating tags
+#' @details Get the Dockerfile template which is annotated 
+#'     with whisker templating tags.
 #' 
 .dockerfile_template <-
     function()
@@ -33,20 +34,26 @@ get_rocker_rstudio_version <-
 
 #' Fill in the template with a payload.
 #'
+#' @details `create_payload()` fills in the template with
+#'     the appropriate payload. The payload can be either
+#'     for release or devel.
+#'     
+#' @param `character(1)` version for Bioconductor is 'devel' or 'release'
 #' 
-.create_payload <- function(devel) {
-    ## Argument validitity check
-    stopifnot(is.logical(devel))
-
+.create_payload <- function(version = c("devel", "release")) {
+    
+    version = match.arg(version)
+    
+    ## TODO: The build_vars && symbols don't get read properly. Fix this.
     build_vars <- readLines(
-        system.file("extdata", "build_vars", package="BiocDockerManager")
+        system.file("extdata", "build_vars", package="BiocDockerManager"),
+        encoding = "UTF-8"
     )
-
+    
     ## Create payload
     payload <- list(
-        rocker_rstudio_version = if (devel) {get_rocker_rstudio_version("devel")}
-                                 else {get_rocker_rstudio_version("release")}
-        devel_build_variables = if(devel) build_vars
+        rocker_rstudio_version = .get_rocker_rstudio_version(version),
+        devel_build_variables = if(version == "devel") build_vars
     )
     payload
 }
@@ -54,17 +61,22 @@ get_rocker_rstudio_version <-
 
 #' Render the template with the 
 #'
+#' @details `render_dockerfile()` will render the 
+#'     dockerfile from the template with the appropriate
+#'     information for release or devel. 
+#'
+#' @param `character(1)` version for Bioconductor is 'devel' or 'release'
+#' 
+#' @param `character(1)` path to where the rendered file is written. 
+#' 
 #' @importFrom whisker whisker.render
 #' @export
-render_dockerfile <- function(devel, path) {
+render_dockerfile <- 
+    function(version=c("devel", "release"), path)
+{
     tmpl <- .dockerfile_template()
-
-    if (devel)
-        payload <- .create_payload(devel = TRUE)
-    else
-        payload <- .create_payload(devel = FALSE)
+    payload <- .create_payload(version = version)
     ## Render devel
     writeLines(whisker.render(tmpl, payload), path)
 }
-
 
